2ce754a07f1b0f46dbe5936045cfac747f63c590
Alex-Guima <alexandreagtaveira@gmail.com>
2023-10-02 14:36:44 -0300

    Fix Missing styles.post on article className

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,7 +2,7 @@ import styles from './Post.module.css'
 
 export function Post() {
     return (
-    <article>
+    <article className={styles.post}>
         <header>
             <div className={styles.author}>
                 <img className={styles.avatar} src="https://github.com/Duds29.png"/>
